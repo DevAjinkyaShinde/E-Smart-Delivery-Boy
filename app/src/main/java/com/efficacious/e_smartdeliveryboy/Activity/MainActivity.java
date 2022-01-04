dbae97f92d0273a5a3372e72f2e89f122d4c297d
DevAjinkyaShinde <ajinkya.efficacious@gmail.com>
2022-01-04 11:28:00 +0530

    First commit

---
+++ app/src/main/java/com/efficacious/e_smartdeliveryboy/Activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.efficacious.e_smartdeliveryboy.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.efficacious.e_smartdeliveryboy.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
